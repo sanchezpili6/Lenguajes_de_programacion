d253ba6ca2866e47068a560ea4723684ba95b209
Pili Sánchez <sanchezpili6@gmail.com>
2021-02-08 14:42:26 -0600

    factorial recursivo e iterativo

---
+++ factorial/main.py
@@ -0,0 +1,20 @@
+def factorial_iterativo(number):
+    fact = 1
+    for i in range(1, number +1):
+        fact *= i
+    return fact
+
+def factorial_recursivo(number):
+    if number == 0:
+        return 1
+    else:
+        return number*factorial_recursivo(number-1)
+
+number = int(input("Número: "))
+
+if number>=0:
+    print('El factorial recursivo es: ', factorial_recursivo(number))
+    print('El factorial iterativo es: ', factorial_iterativo(number))
+
+else:
+    print('Número no válido')
