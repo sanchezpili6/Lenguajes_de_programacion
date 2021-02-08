1c921b483fe9d171ad22dbfa4f6720f7d02b19e3
Pili Sánchez <sanchezpili6@gmail.com>
2021-02-08 14:33:32 -0600

    Initial commit

---
+++ numberGuessingGame/main.py
@@ -0,0 +1,27 @@
+import random
+
+print('set the lower bound:')
+lowerBound = int(input())
+print('set the higher bound:')
+higherBound = int(input())
+
+guesses = 0
+
+number = random.randint(lowerBound, higherBound)
+
+for guesses in range (5):
+    print('Guess the number: ')
+    guess = int(input())
+
+    if guess < number:
+        print('Try a higher number')
+
+    if guess > number:
+        print('Try a lower number')
+
+    if guess == number:
+        print(f'Your guess is correct! it took you ',guesses + 1 ,' guesses')
+        break
+
+if guess != number:
+    print(f'The correct number was: ', number)
